c8fb815f43114f13811c8ab7bf1326cf8cbca8fb
Pierre-Marie Fantchenou <pfantche@utbm.fr>
2020-03-13 17:59:36 +0100

    First push

---
+++ src/main/groovy/gl51/data/Photo.java
@@ -0,0 +1,5 @@
+package gl51.data;
+
+public class Photo {
+    
+}
---
+++ src/main/groovy/gl51/service/IPServiceImpl.groovy
@@ -0,0 +1,12 @@
+package gl51.service
+
+import gl51.data.Ip
+
+
+@Singleton
+class IPServiceImpl implements IPService {
+    @Override
+    Ip fetchIp() {
+        return null
+    }
+}

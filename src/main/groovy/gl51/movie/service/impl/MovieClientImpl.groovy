395f6db1b93b5c78f12d8682eeaf38c3addbdee2
PmRay <fantchenou.pierremarie@gmail.com>
2020-03-27 18:00:23 +0100

    Mise en place du test

---
+++ src/main/groovy/gl51/movie/service/impl/MovieClientImpl.groovy
@@ -0,0 +1,10 @@
+package gl51.movie.service.impl
+
+import gl51.movie.data.Movie
+
+class MovieClientImpl implements gl51.movie.service.MovieClient {
+    @Override
+    Movie getMovieDetail(String imdbID) {
+        return null
+    }
+}

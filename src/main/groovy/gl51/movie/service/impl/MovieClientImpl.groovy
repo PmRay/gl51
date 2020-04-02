66f40433d69cafdfbc2f9ffe842c5a358a3950fb
PmRay <fantchenou.pierremarie@gmail.com>
2020-04-02 23:47:46 +0200

    Mock&Workflow

--- src/main/groovy/gl51/movie/service/impl/MovieClientImpl.groovy
+++ src/main/groovy/gl51/movie/service/impl/MovieClientImpl.groovy
@@ -1,10 +1,24 @@
 package gl51.movie.service.impl
 
 import gl51.movie.data.Movie
+import gl51.movie.service.MovieClient
+import gl51.movie.service.MovieRegistry
 
-class MovieClientImpl implements gl51.movie.service.MovieClient {
+import javax.inject.Inject
+
+@Singleton
+class MovieClientImpl implements MovieClient {
+    @Inject MovieRegistry registry
+
     @Override
     Movie getMovieDetail(String imdbID) {
+        if(!registry.listFavorites().isEmpty()) {
+            for(Movie movie : registry.listFavorites()) {
+                if(imdbID == movie.getImdbID()) {
+                    return movie
+                }
+            }
+        }
         return null
     }
 }
--- src/test/groovy/gl51/movie/service/impl/MovieRegistryImplTest.groovy
+++ src/test/groovy/gl51/movie/service/impl/MovieRegistryImplTest.groovy
@@ -1,14 +1,16 @@
 package gl51.movie.service.impl
 
+import gl51.movie.service.MovieClient
 import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.annotation.MockBean
 import spock.lang.Specification
 
 import javax.inject.Inject
 
 @MicronautTest
 class MovieRegistryImplTest extends Specification {
-    @Inject
-    MovieRegistryImpl registry
+    @Inject MovieRegistryImpl registry
+    @Inject MovieClientImpl client
 
     void "injection should work"() {
         expect:
@@ -22,8 +24,14 @@ class MovieRegistryImplTest extends Specification {
 
     void "adding a facovite should fill in the database"() {
         when:
-        registry.addMovieToFavorites("aaapppaa")
+        registry.addMovieToFavorites("abcde")
         then:
         registry.listFavorites().size() == 1
+        client.getMovieDetail("abcde")!=null
+        client.getMovieDetail("aaaa") == null
+    }
+    @MockBean(MovieClientImpl)
+    MovieClient client() {
+        Mock(MovieClient)
     }
 }
